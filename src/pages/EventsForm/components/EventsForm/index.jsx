bc3072579c7b5fb13a4d9bda52e88e2f03c67c3a
Daphne Won <daphne.won@shopee.com>
2019-06-21 14:15:04 +0800

    Partially done with filling up form ( for CT to see )

--- src/pages/EventsForm/components/EventsForm/index.jsx
+++ src/pages/EventsForm/components/EventsForm/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import sanitizeHtml from 'sanitize-html';
 import './index.scss';
 
 import {
@@ -6,15 +7,20 @@ import {
   Input,
   Tooltip,
   Icon,
+  Row,
   Col,
   Button,
+  DatePicker,
+  TimePicker,
+  Modal
 } from 'antd';
 
 const { TextArea } = Input;
+const { RangePicker } = DatePicker;
 
 class RegistrationForm extends React.Component {
   state = {
-    confirmDirty: false,
+    isPreview: false,
   };
 
   handleSubmit = e => {
@@ -26,41 +32,12 @@ class RegistrationForm extends React.Component {
     });
   };
 
-  handleConfirmBlur = e => {
-    const value = e.target.value;
-    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
-  };
-
-  compareToFirstPassword = (rule, value, callback) => {
-    const form = this.props.form;
-    if (value && value !== form.getFieldValue('password')) {
-      callback('Two passwords that you enter is inconsistent!');
-    } else {
-      callback();
-    }
-  };
-
-  validateToNextPassword = (rule, value, callback) => {
-    const form = this.props.form;
-    if (value && this.state.confirmDirty) {
-      form.validateFields(['confirm'], { force: true });
-    }
-    callback();
-  };
-
-  handleWebsiteChange = value => {
-    let autoCompleteResult;
-    if (!value) {
-      autoCompleteResult = [];
-    } else {
-      autoCompleteResult = ['.com', '.org', '.net'].map(domain => `${value}${domain}`);
-    }
-    this.setState({ autoCompleteResult });
-  };
-
   render() {
     const { getFieldDecorator } = this.props.form;
-    const { autoCompleteResult } = this.state;
+
+    const config = {
+      rules: [{ type: 'object', required: true, message: 'Please select time!' }],
+    };
 
     const formItemLayout = {
       labelCol: {
@@ -92,16 +69,7 @@ class RegistrationForm extends React.Component {
         {/* name */}
         <Form {...formItemLayout} onSubmit={this.handleSubmit}>
           <Form.Item
-            label="name"
-            className="formItemOverride"
-            label={
-              <span>
-                Event Name&nbsp;
-                <Tooltip title="The title where everyone will see.">
-                  <Icon type="question-circle-o" />
-                </Tooltip>
-              </span>
-            }
+            label="Event Name"
           >
             <Col span={colSpan}>
             {getFieldDecorator('name', {
@@ -116,16 +84,7 @@ class RegistrationForm extends React.Component {
           </Form.Item>
           {/* abstract */}
           <Form.Item
-            label="abstract"
-            className="formItemOverride"
-            label={
-              <span>
-                Abstract&nbsp;
-                <Tooltip title="The summary of your event">
-                  <Icon type="question-circle-o" />
-                </Tooltip>
-              </span>
-            }
+            label="Abstract"
           >
             <Col span={colSpan}>
             {getFieldDecorator('abstract', {
@@ -140,17 +99,16 @@ class RegistrationForm extends React.Component {
           </Form.Item>
           {/* Introduction */}
           <Form.Item
-            label="abstract"
-            className="formItemOverride"
             label={
               <span>
                 Event Introduction&nbsp;
-                <Tooltip title="The summary of your event">
+                <Tooltip title="Display of information in HTML">
                   <Icon type="question-circle-o" />
                 </Tooltip>
               </span>
             }
           >
+            <Row gutter={8}>
             <Col span={colSpan}>
             {getFieldDecorator('introduction', {
               rules: [
@@ -167,6 +125,26 @@ In the second part of the Shopee Xplores! series, we will embark on a scenic cyc
 
 Come along to enjoy the fun!`} autosize />)}
             </Col>
+            <Col span={colSpan}>
+              <Button
+                onClick={() => {
+                  this.setState({
+                    isPreview: true,
+                  });
+                }}
+              >
+                Preview
+              </Button>
+            </Col>
+            </Row>
+          </Form.Item>
+          {/* Event start time */}
+          <Form.Item
+            label="Event Start Time"
+          >
+            {getFieldDecorator('date-time-picker', config)(
+              <DatePicker className="dateTimePicker" showTime format="YYYY-MM-DD HH:mm:ss" />,
+            )}
           </Form.Item>
           <Form.Item {...tailFormItemLayout}>
             <Button type="primary" htmlType="submit">
@@ -174,6 +152,69 @@ Come along to enjoy the fun!`} autosize />)}
             </Button>
           </Form.Item>
         </Form>
+        <Modal
+          title={`${this.props.form.getFieldValue('name') || 'Event'}'s Introduction Preview`}
+          centered
+          visible={this.state.isPreview}
+          onCancel={() => this.setState({isPreview: false})}
+          footer={[
+             <Button key="submit" type="primary" onClick={() => this.setState({isPreview: false})}>
+               Return
+             </Button>,
+           ]}
+        >
+          {(() => {
+            const shaf = sanitizeHtml(this.props.form.getFieldValue('introduction'), {
+              allowedTags: [
+                'h3',
+                'h4',
+                'h5',
+                'h6',
+                'blockquote',
+                'p',
+                'a',
+                'ul',
+                'ol',
+                'nl',
+                'li',
+                'b',
+                'i',
+                'strong',
+                'span',
+                'em',
+                'strike',
+                'code',
+                'hr',
+                'br',
+                'div',
+                'table',
+                'thead',
+                'caption',
+                'tbody',
+                'tr',
+                'th',
+                'td',
+                'pre',
+                'font',
+                'font face',
+                'col',
+                'colgroup',
+
+              ],
+              allowedAttributes: {
+                font: ['color'],
+                div: ['style'],
+                img: ['src'],
+                a: ['href'],
+                span: ['style'],
+                table: ['style'],
+                col: ['width', 'height'],
+                td: ['style'],
+              },
+            });
+            return <div dangerouslySetInnerHTML={{ __html: shaf }} />;
+          })()}
+        </Modal>
       </React.Fragment>
     );
   }
--- src/pages/EventsForm/index.jsx
+++ src/pages/EventsForm/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.scss';
-import EventsForm from '../../components/EventsForm';
+import EventsForm from './components/EventsForm';
 
 export default function() {
   return (
